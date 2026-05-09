e74a050a44adffad7a718ea703578c04b80fcca8
firoj7github <fkfiroj2024@gmail.com>
2026-05-09 15:08:22 +0600

    docker-compose-resetup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App" style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
       <header className="App-header">
         <h1>Hydrus Digital BD</h1>
-        <p>Frontend is running with <strong>Nginx</strong></p>
-        <div style={{ padding: '20px', background: '#f4f4f4', display: 'inline-block', borderRadius: '8px' }}>
+        <p>Frontend is running</p>
+        <div style={{ padding: '20px', background: '#f4f4f4', color: 'black', display: 'inline-block', borderRadius: '8px' }}>
           <strong>System Status:</strong> {status}
         </div>
       </header>
